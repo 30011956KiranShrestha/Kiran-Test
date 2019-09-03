714f751255a0600ee751c21790e2c2632dbeff8b
Kiran Shrestha <30011956@student.toiohomai.ac.nz>
2019-09-04 11:44:39 +1200

    test

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 2(x+3) + x = 12
+            // 2x + 6 + x = 12
+            // 3x = 12-6
+            // 3x = 6
+            // x = 2
+
+            Console.WriteLine("------------------------------------------------------------------");
+            Console.WriteLine("-------can you solve the following algebraic expression-----------");
+            Console.WriteLine("------------------------------------------------------------------");
+
+            Console.Write("\nEnter your guess at the value of X: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            if (num1 == 2)
+                Console.Write("\nYour guess is correct");
+            else
+                Console.Write("\nSorry that guess is incorrect");
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****************************************************************");
+            Console.WriteLine("**********This program give the result of Base^Exponent**********");
+            Console.WriteLine("*****************************************************************");
+            Console.Write("\nEnter the value of base: ");
+            Double bNum = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nEnter the value of Exponent: ");
+            int pwr = Convert.ToInt32(Console.ReadLine());
+            result=CalcuOfPower(bnum, pwr);
+
+
+            Console.Write("\nThe value of {0} to the power of {1} is : {2} \n\n}
+     
+
+        }
+    }
+}
