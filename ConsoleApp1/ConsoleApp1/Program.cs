b6003c3a67a785ba3d79d407b157f1208e8bac62
Kiran Shrestha <30011956@student.toiohomai.ac.nz>
2019-09-10 12:46:51 +1200

    test

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,7 +10,22 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-           
+            Console.WriteLine("*********************************************************************");
+            Console.WriteLine("************This Program  give the sum of total of price*************");
+            Console.WriteLine("*********************************************************************");
+            Console.WriteLine("\nHello");
+            Double total = 0;
+            for (int i = 1; i <=5; i++)
+
+            {
+                Console.Write($"\nEnter the price of item {i}: ");
+                total += double.Parse(Console.ReadLine());
+
+
+            }
+            Console.WriteLine($"\nTotal of all items is: ${total} ");
+            Console.ReadLine();
+
 
 
 
